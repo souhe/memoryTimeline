a9d3f9af69e60f382336534ba8e1a439b2268ca2
souhe <souhepend@hotmail.com>
2016-02-23 23:35:35 +0100

    Added new gulp task 'serve' which starts server and waches files (after every change it reloads server)

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,32 @@ var concat = require('gulp-concat');
 var webpack = require('webpack-stream');
 var webpackConfig = require('./webpack.config.js');
 var connect = require('gulp-connect');
+var gls = require('gulp-live-server');
+var notifier = require('node-notifier');
 
-gulp.task('build', function(){
+function build(){
     return gulp.src('src/main.js')
         .pipe(webpack(webpackConfig))
         .pipe(concat('main.js'))
         .pipe(gulp.dest('dist'));
+}
+
+gulp.task('serve', function() {
+
+    var server = gls.new('./src/server.babel.js');    
+    server.start();
+    
+    gulp.watch(['src/**/*.js', 'src/**/*.less'], ['build', function (file) {
+        server.start.bind(server)();
+        notifier.notify({
+            title: 'built',
+            message: 'server reloaded'
+        });
+    }]); 
+});
+
+gulp.task('build', function(){
+    return build();
 });
 
 gulp.task('copy', function() {
